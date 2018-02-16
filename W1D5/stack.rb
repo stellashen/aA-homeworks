da7f8113854bf8075062f4380980248a9f7f2b41
Yangchen Shen <shyangch@gmail.com>
2018-02-15 22:40:21 -0800

    Finish Stack class

--- W1D5/stack.rb
+++ W1D5/stack.rb
@@ -1,17 +1,23 @@
 class Stack
+  attr_accessor :stack
+
   def initialize
     # create ivar to store stack here!
+    @stack = []
   end
 
   def add(el)
     # adds an element to the stack
+    stack.push(el)
   end
 
   def remove
     # removes one element from the stack
+    stack.pop
   end
 
   def show
     # return a copy of the stack
+    stack.dup
   end
 end
