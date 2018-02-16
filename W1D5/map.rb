4cd4ddbe166f7ffd461239c7c9900cba55700996
Yangchen Shen <shyangch@gmail.com>
2018-02-15 23:23:11 -0800

    Implement map using array instead of hash

--- W1D5/map.rb
+++ W1D5/map.rb
@@ -1,22 +1,32 @@
 class Map
   def initialize
-    @map = {}
+    @map = [] # holds [key,value] pairs
   end
 
   def assign(key, value)
-    @map[key] = value if @map[key] == nil
-    @map[key] << value
+    idx = @map.index { |pair| pair[0] == key }
+    # overwrite value if key exists, otherwise insert key, value pair
+    idx ? @map[idx][1] = value : @map.push([key, value])
+    [key, value]
   end
 
   def lookup(key)
-    @map[key]
+    @map.each { |pair| return pair[1] if pair[0] == key}
+    nil
   end
 
   def remove(key)
-    @map.delete(key)
+    @map.reject! { |pair| pair[0] == key }
+    nil
   end
 
   def show
-    @map.dup
+    deep_dup(@map)
+  end
+
+  private
+
+  def deep_dup(arr)
+    arr.map { |el| el.is_a?(Array) ? deep_dup(arr) : el}
   end
 end
