b2d31228b603f61ecaa225dca05dcc49585f39db
Yangchen Shen <shyangch@gmail.com>
2018-02-18 10:58:00 -0800

    Pass all specs

--- W2D1/Simon/lib/simon.rb
+++ W2D1/Simon/lib/simon.rb
@@ -4,19 +4,29 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 
   def play
-
+    until game_over
+      take_turn
+    end
+    game_over_message
+    reset_game
   end
 
   def take_turn
-
+    show_sequence
+    require_sequence
+    @sequence_length += 1
+    round_success_message unless game_over
   end
 
   def show_sequence
-
+    add_random_color
+    puts seq.to_s
   end
 
   def require_sequence
@@ -24,7 +34,7 @@ class Simon
   end
 
   def add_random_color
-
+    seq << COLORS.sample
   end
 
   def round_success_message
@@ -36,6 +46,8 @@ class Simon
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 end
