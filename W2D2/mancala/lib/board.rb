edc47e324c84ac41759f2a1db6058e2edcd9dc76
Yangchen Shen <shyangch@gmail.com>
2018-02-19 23:17:49 -0800

    Change opponent cups definition under make_move method

--- W2D2/mancala/lib/board.rb
+++ W2D2/mancala/lib/board.rb
@@ -25,7 +25,10 @@ class Board
   end
 
   def make_move(start_pos, current_player_name)
-    opponent_store_pos = start_pos < 7 ? 13 : 6
+    opponent_side = (0..6).to_a
+    opponent_side = (7..13).to_a if start_pos < 7
+    # opponent_store_pos = start_pos < 7 ? 13 : 6
+
     num_stones = @cups[start_pos].length
     # 1. empty the selected cup
     @cups[start_pos] = Array.new
@@ -34,15 +37,15 @@ class Board
     num_placed = 0
     idx_to_place_stone = start_pos + 1
     while num_placed < num_stones
-      unless idx_to_place_stone == opponent_store_pos
+      unless opponent_side.include?(idx_to_place_stone)
         distribute_stone(idx_to_place_stone)
         num_placed += 1
       end
-      idx_to_place_stone += 1
+      idx_to_place_stone += 1 unless num_placed == num_stones
     end
 
     # 3. display board
-    render
+    # render
 
     # 4. ending situations: if the ending cup
     # a) has stones already in it
@@ -51,22 +54,12 @@ class Board
     # => returns :switch
     # c) is an empty cup
     # => returns :prompt
-    next_turn(idx_to_place_stone - 1)
-  end
-
-  def distribute_stone(idx)
-    # helper method to determine what #make_move returns
-    cup = @cups[idx]
-    if cup.nil?
-      @cups[idx] = [:stone]
-    else
-      @cups[idx] << :stone
-    end
+    next_turn(idx_to_place_stone)
   end
 
   def next_turn(ending_cup_idx)
     # helper method to determine what #make_move returns
-    return :prompt if [6, 13].include? (ending_cup_idx)
+    return :prompt if [6, 13].include?(ending_cup_idx)
     return :switch if @cups[ending_cup_idx].length == 1
     ending_cup_idx
   end
@@ -83,5 +76,17 @@ class Board
   end
 
   def winner
+  end
+
+  private
+
+  def distribute_stone(idx)
+    # helper method to determine what #make_move returns
+    cup = @cups[idx]
+    if cup.nil?
+      @cups[idx] = [:stone]
+    else
+      @cups[idx] << :stone
+    end
   end
 end
