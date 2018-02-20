c53b301493370ef8ec26d63f50acda300c3597bf
Yangchen Shen <shyangch@gmail.com>
2018-02-19 23:45:59 -0800

    Fix valid_move? and make_move bugs

--- W2D2/mancala/lib/board.rb
+++ W2D2/mancala/lib/board.rb
@@ -3,8 +3,8 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
-    @player1 = Player.new(name1, 1)
-    @player2 = Player.new(name2, 2)
+    @name1 = name1
+    @name2 = name2
     @cups = Array.new(14) { [] }
     place_stones
   end
@@ -22,12 +22,15 @@ class Board
 
   def valid_move?(start_pos)
     raise "Invalid starting cup" unless (1..14).cover?(start_pos)
+    raise "Invalid starting cup" if @cups[start_pos].empty?
   end
 
   def make_move(start_pos, current_player_name)
-    opponent_side = (0..6).to_a
-    opponent_side = (7..13).to_a if start_pos < 7
-    # opponent_store_pos = start_pos < 7 ? 13 : 6
+    if current_player_name == @name1
+      opponent_store = 13
+    else
+      opponent_store = 6
+    end
 
     num_stones = @cups[start_pos].length
     # 1. empty the selected cup
@@ -37,15 +40,16 @@ class Board
     num_placed = 0
     idx_to_place_stone = start_pos + 1
     while num_placed < num_stones
-      unless opponent_side.include?(idx_to_place_stone)
+      unless opponent_store == idx_to_place_stone
         distribute_stone(idx_to_place_stone)
         num_placed += 1
       end
       idx_to_place_stone += 1 unless num_placed == num_stones
+      idx_to_place_stone = 0 if idx_to_place_stone > 13
     end
 
     # 3. display board
-    # render
+    render
 
     # 4. ending situations: if the ending cup
     # a) has stones already in it
