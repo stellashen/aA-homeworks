6367f63c8751fa44c42bf8789c8fb0b62c330328
Yangchen Shen <shyangch@gmail.com>
2018-02-27 09:01:32 -0800

    Fix bugs in Play class

--- W3D2/ORM/plays.rb
+++ W3D2/ORM/plays.rb
@@ -19,6 +19,36 @@ class Play
     data.map { |datum| Play.new(datum) }
   end
 
+  def self.find_by_title(title)
+    play = PlayDBConnection.instance.execute(<<-SQL, title)
+      SELECT
+        *
+      FROM
+        plays
+      WHERE
+        title = ?
+    SQL
+    return nil unless play.length > 0
+
+    Play.new(play.first) # play is stored in an array
+  end
+
+  def self.find_by_playwright(name)
+    playwright = Playwright.find_by_name(name)
+    raise "#{name} not found in DB" unless playwright
+
+    plays = PlayDBConnection.instance.execute(<<-SQL, name)
+      SELECT
+        *
+      FROM
+        plays
+      WHERE
+        playwright_id = ?
+    SQL
+
+    plays.map { |play| Play.new(play) }
+  end
+
   def initialize(options)
     @id = options['id']
     @title = options['title']
@@ -46,24 +76,6 @@ class Play
         title = ?, year = ?, playwright_id = ?
       WHERE
         id = ?
-    SQL
-  end
-
-  def self.find_by_title(title)
-    PlayDBConnection.instance.execute(<<-SQL, title)
-      SELECT
-        *
-      WHERE
-        title = ?
-    SQL
-  end
-
-  def self.find_by_name(name)
-    PlayDBConnection.instance.execute(<<-SQL, name)
-      SELECT
-        *
-      WHERE
-        name = ?
     SQL
   end
 end
