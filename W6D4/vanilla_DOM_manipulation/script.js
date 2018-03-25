c6e43facaec5e4b1ca6dce08cd07d958814c6f65
Yangchen Shen <shyangch@gmail.com>
2018-03-24 21:25:10 -0700

    Write event listeners for adding list items and photos

--- W6D4/vanilla_DOM_manipulation/script.js
+++ W6D4/vanilla_DOM_manipulation/script.js
@@ -18,14 +18,54 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // adding SF places as list items
 
-  // --- your code here!
+  const handleFavoriteSubmit = (e) => {
+  e.preventDefault();
 
+  const favoriteInput = document.querySelector(".favorite-input");
+  const favorite = favoriteInput.value;
+  favoriteInput.value = "";
 
+  const newListLi = document.createElement("li");
+  newListLi.textContent = favorite;
+
+  const favoritesList = document.getElementById("sf-places");
+  favoritesList.appendChild(newListLi);
+  };
+
+  const listSubmitButton = document.querySelector(".favorite-submit");
+  listSubmitButton.addEventListener("click", handleFavoriteSubmit);
 
   // adding new photos
+  const showPhotoForm = (e) => {
+    const photoFormDiv = document.querySelector(".photo-form-container");
+    if (photoFormDiv.className === "photo-form-container") {
+      photoFormDiv.className = "photo-form-container hidden";
+    } else {
+      photoFormDiv.className = "photo-form-container";
+    }
+  };
 
-  // --- your code here!
+  const photoFormShowButton = document.querySelector(".photo-show-button");
+  photoFormShowButton.addEventListener("click", showPhotoForm);
 
 
+  const handlePhotoSubmit = (e) => {
+    e.preventDefault();
 
-});
+    const photoUrlInput = document.querySelector(".photo-url-input");
+    const photoUrl = photoUrlInput.value;
+    photoUrlInput.value = "";
+
+    const newImg = document.createElement("img");
+    newImg.src = photoUrl;
+
+    const newPhotoLi = document.createElement("li");
+    newPhotoLi.appendChild(newImg);
+
+    const dogPhotosList = document.querySelector(".dog-photos");
+    dogPhotosList.appendChild(newPhotoLi);
+  };
+
+  const photoSubmitButton = document.querySelector(".photo-url-submit");
+  photoSubmitButton.addEventListener("click", handlePhotoSubmit);
+  });
