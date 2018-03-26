bfcd129548c27e9da1ddb00f42cf6c8b728e66c5
Yangchen Shen <shyangch@gmail.com>
2018-03-26 01:27:47 -0700

    Complete Phase2: refactor

--- W7D2/middleware/frontend/todo_redux.jsx
+++ W7D2/middleware/frontend/todo_redux.jsx
@@ -10,19 +10,38 @@ document.addEventListener('DOMContentLoaded', () => {
   let store = configureStore(preloadedState);
 
   // Phase 1: Logging
-  store.dispatch = addLoggingToDispatch(store);
+  // store.dispatch = addLoggingToDispatch(store);
+
+  // Phase 2: Refactoring (without using Redux applyMiddleware)
+  store = applyMiddlewares(store, addLoggingToDispatch);
 
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
 });
 
 // Phase 1: Logging
-const addLoggingToDispatch = (store) => {
-  const OGDispatch = store.dispatch;
-  return (action) => {
-    console.log(store.getState());
-    console.log(action);
-    OGDispatch(action);
-    console.log(store.getState());
-  };
+// const addLoggingToDispatch = (store) => {
+//   const OGDispatch = store.dispatch;
+//   return (action) => {
+//     console.log(store.getState());
+//     console.log(action);
+//     OGDispatch(action);
+//     console.log(store.getState());
+//   };
+// };
+
+// Phase 2: Refactoring (without Using Redux applyMiddleware)
+const addLoggingToDispatch = store => next => action => {
+  console.log(store.getState());
+  console.log(action);
+  next(action);
+  console.log(store.getState());
+};
+
+const applyMiddlewares = (store, ...middlewares) => {
+  let dispatch = store.dispatch;
+  middlewares.forEach((middleware) => {
+    dispatch = middleware(store)(dispatch);
+  });
+  return Object.assign({}, store, { dispatch });
 };
